8054111629baa26ae7c699a3a822b60ebfc76659
eori_mumble <enrico.ori@mumbleideas.it>
2020-07-07 16:40:19 +0200

    Latest fixes

--- app/src/main/java/mumble/mburger/mbaudience/MBAudienceData/MBTag.kt
+++ app/src/main/java/mumble/mburger/mbaudience/MBAudienceData/MBTag.kt
@@ -2,4 +2,5 @@ package mumble.mburger.mbaudience.MBAudienceData
 
 import java.io.Serializable
 
+/**Tag for user that can be associated**/
 class MBTag(var key: String, var value: String) : Serializable
